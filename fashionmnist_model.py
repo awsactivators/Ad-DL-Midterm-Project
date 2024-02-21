39f27605dc3a664ffbec835ab840c07ea182882f
Noah Nsimbe <37845280+NoahNsimbe@users.noreply.github.com>
2024-02-21 12:45:43 -0500

    cleanup

--- fashionmnist_model.py
+++ fashionmnist_model.py
@@ -1,73 +1,93 @@
-import pandas as pd
-from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras.datasets import fashion_mnist
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
 
-def load_data():
-    (X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
-    return X_train, y_train, X_test, y_test
+class FMM:
+    @staticmethod
+    def load_data():
+        fashion_mnist = tf.keras.datasets.fashion_mnist
+        (X_train, y_train), (
+            X_test,
+            y_test,
+        ) = fashion_mnist.load_data()
+        return X_train, y_train, X_test, y_test
 
+    @staticmethod
+    def create_model():
+        return tf.keras.Sequential(
+            [
+                tf.keras.layers.Flatten(input_shape=(28, 28)),
+                tf.keras.layers.Dense(256, activation="relu"),
+                tf.keras.layers.Dense(64, activation="relu"),
+                tf.keras.layers.Dense(10, activation="softmax"),
+            ]
+        )
 
-def create_model():
-    model = keras.Sequential(
-        [
-            layers.Dense(128, activation="relu", input_shape=(784,)),
-            layers.Dropout(0.3),
-            layers.BatchNormalization(),
-            layers.Dense(24, activation="relu"),
-            layers.Dropout(0.3),
-            layers.BatchNormalization(),
-            layers.Dense(24, activation="relu"),
-            layers.Dropout(0.3),
-            layers.BatchNormalization(),
-            layers.Dense(10, activation="softmax"),
-        ]
-    )
+    #         return tf.keras.Sequential(
+    #             [
+    #             tf.keras.layers.Flatten(input_shape=(28, 28)),
+    #             tf.keras.layers.Dense(256, activation="relu"),
+    #             tf.keras.layers.Dropout(0.3),
+    #             tf.keras.layers.BatchNormalization(),
+    #             tf.keras.layers.Dense(256, activation="relu"),
+    #             tf.keras.layers.Dropout(0.3),
+    #             tf.keras.layers.BatchNormalization(),
+    #             tf.keras.layers.Dense(256, activation="relu"),
+    #             tf.keras.layers.Dropout(0.3),
+    #             tf.keras.layers.BatchNormalization(),
+    #             tf.keras.layers.Dense(10, activation="softmax"),
+    #             ]
+    #         )
 
-    return model
+    @staticmethod
+    def reshape_data(X_train, X_test):
+        return X_train / 255.0, X_test / 255.0
 
-
-def compile_and_train(
-    model,
-    X_train,
-    y_train,
-    X_test,
-    y_test,
-    optimizer,
-    batch_size=100,
-    epochs=30,
-    validation_split=0.2,
-):
-    model.compile(
-        loss="sparse_categorical_crossentropy",
-        optimizer=optimizer,
-        metrics=["accuracy", "mae"],
-    )
-    history = model.fit(
+    @staticmethod
+    def compile_and_train(
+        model,
         X_train,
         y_train,
-        batch_size=batch_size,
-        epochs=epochs,
-        validation_split=validation_split,
-    )
+        X_test,
+        y_test,
+        optimizer,
+        batch_size=100,
+        epochs=60,
+    ):
+        model.compile(
+            loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+            optimizer=optimizer,
+            metrics=["accuracy"],
+        )
+        history = model.fit(
+            X_train,
+            y_train,
+            batch_size=batch_size,
+            epochs=epochs,
+            shuffle=True,
+            validation_data=(X_test, y_test),
+        )
 
-    # Evaluate the model on the test set
-    test_loss, test_acc, test_mae = model.evaluate(X_test, y_test)
-    print(f"Test loss: {test_loss}, Test accuracy: {test_acc}, Test MAE: {test_mae}")
+        train_accuracy = history.history["accuracy"][-1]
+        val_accuracy = history.history["val_accuracy"][-1]
 
-    return history
+        return history, train_accuracy, val_accuracy
 
+    @staticmethod
+    def evaluate(
+        model,
+        X_test,
+        y_test,
+    ):
+        loss, accuracy = model.evaluate(X_test, y_test, verbose=2)
+        return loss, accuracy
 
-def evaluate_model(model, X_test, y_test):
-    test_loss, test_acc, test_mae = model.evaluate(X_test, y_test)
-    print(f"Test loss: {test_loss}, Test accuracy: {test_acc}, Test MAE: {test_mae}")
-
-    return test_loss, test_acc, test_mae
-
-
-def reshape_data(X_train, X_test):
-    x_train, x_test = X_train / 255.0, X_test / 255.0
-    x_train_flattened = x_train.reshape(x_train.shape[0], -1)
-    x_test_flattened = x_test.reshape(x_test.shape[0], -1)
-    return x_train_flattened, x_test_flattened
+    @staticmethod
+    def plot_history(history, optimizer):
+        plt.plot(history.history["loss"], label="Training loss")
+        plt.plot(history.history["val_loss"], label="Validation loss")
+        plt.title(f"Training and Validation Loss ({optimizer.__class__.__name__})")
+        plt.xlabel("Epochs")
+        plt.ylabel("Loss")
+        plt.legend()
+        plt.show()
