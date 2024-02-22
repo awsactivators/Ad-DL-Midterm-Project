706978bea752223cb8510d457e3fedb5caf94386
Noah Nsimbe <37845280+NoahNsimbe@users.noreply.github.com>
2024-02-22 14:21:23 -0500

    cleanup

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Ad-DL-Midterm-Project
 
-### Dataset
+## Dataset
+
 We have the Fashion MNIST which is also a large database on clothing apparels. Our work is to use three different optimizer algorithms (excluding vanilla GD/SGD) to figure out the details, pros, and cons of each one by applying all three algorithms to the artificial neural network (ANN) model to identify clothing accessories.
 
-### Deep Learning Model - ANN
+## Deep Learning Model - ANN
+
 Artificial Neural network resembles the brain's neural network with densely connected neurons in between input and output layers. It has a hidden layers where the internal processing happens in ANN. The neural network's objective is to minimise the loss(actual-predicted) by using the learning method called as back propogation where the weights get re-initialized in each connecting layer for many epochs through which the loss is minimised.
 
 We will build and compile an ANN model without any hyperparameter tuning for the model accuracy
@@ -21,11 +23,10 @@ The base model of neural network is ready. It's time to connect the brain for th
 **Fitting the model**
 Now, its time to train our neural network. Since we didn't use any hyperparameter training we used our own values for batch size and epochs.
 
-
 Our project includes one Python (.py) file containing model functions for training, testing, evaluating etc.
 
-Additionally, I provide three Jupyter Notebook (.ipynb) files to facilitate your work. 
+Additionally, I provide three Jupyter Notebook (.ipynb) files to facilitate your work.
 
-- The first 3 notebook, named **fashion_mnist_optimizer*.ipynb**, is specifically set up for experimenting with different optimizers to understand their impact on model performance, each person takes one. 
+- The first 3 notebook, named **fashion_mnist_optimizer*.ipynb**, is specifically set up for experimenting with different optimizers to understand their impact on model performance, each person takes one.
 
 - The last notebook, **fashion_mnist_model_test.ipynb**, focuses on the accuracy of models constructed It is important to note that the evaluation in this notebook is conducted without applying any optimizers, providing a baseline performance metric for each model configuration.
--- fashionmnist_model.py
+++ fashionmnist_model.py
@@ -11,9 +11,9 @@ class FMM:
             y_test,
         ) = fashion_mnist.load_data()
         return X_train, y_train, X_test, y_test
-
+    
     @staticmethod
-    def create_model():
+    def create_model_v1():
         return tf.keras.Sequential(
             [
                 tf.keras.layers.Flatten(input_shape=(28, 28)),
@@ -23,21 +23,46 @@ class FMM:
             ]
         )
 
-    #         return tf.keras.Sequential(
-    #             [
-    #             tf.keras.layers.Flatten(input_shape=(28, 28)),
-    #             tf.keras.layers.Dense(256, activation="relu"),
-    #             tf.keras.layers.Dropout(0.3),
-    #             tf.keras.layers.BatchNormalization(),
-    #             tf.keras.layers.Dense(256, activation="relu"),
-    #             tf.keras.layers.Dropout(0.3),
-    #             tf.keras.layers.BatchNormalization(),
-    #             tf.keras.layers.Dense(256, activation="relu"),
-    #             tf.keras.layers.Dropout(0.3),
-    #             tf.keras.layers.BatchNormalization(),
-    #             tf.keras.layers.Dense(10, activation="softmax"),
-    #             ]
-    #         )
+    @staticmethod
+    def create_model_v2():
+        return tf.keras.Sequential(
+            [
+                tf.keras.layers.Flatten(input_shape=(28, 28)),
+                tf.keras.layers.Dense(128, activation="relu"),
+                tf.keras.layers.BatchNormalization(),
+                tf.keras.layers.Dropout(0.3),
+                tf.keras.layers.Dense(64, activation="relu"),
+                tf.keras.layers.BatchNormalization(),
+                tf.keras.layers.Dropout(0.3),
+                tf.keras.layers.Dense(32, activation="relu"),
+                tf.keras.layers.BatchNormalization(),
+                tf.keras.layers.Dropout(0.3),
+                tf.keras.layers.Dense(10, activation="softmax"),
+            ]
+        )
+
+    @staticmethod
+    def create_model_v3():
+        return tf.keras.Sequential(
+            [
+                tf.keras.layers.Conv2D(32, (3,  3), padding='same', input_shape=(28,  28,  1), activation='relu'),
+                tf.keras.layers.BatchNormalization(),
+                tf.keras.layers.Dropout(0.25),
+                tf.keras.layers.MaxPooling2D(pool_size=(2,  2)),
+
+                tf.keras.layers.Conv2D(64, (3,  3), padding='same', activation='relu'),
+                tf.keras.layers.BatchNormalization(),
+                tf.keras.layers.Dropout(0.25),
+                tf.keras.layers.MaxPooling2D(pool_size=(2,  2)),
+
+                tf.keras.layers.Flatten(),
+                tf.keras.layers.Dense(128, activation='relu'),
+                tf.keras.layers.BatchNormalization(),
+                tf.keras.layers.Dropout(0.25),
+                
+                tf.keras.layers.Dense(10, activation='softmax'),
+            ]
+        )
 
     @staticmethod
     def reshape_data(X_train, X_test):
@@ -48,46 +73,88 @@ class FMM:
         model,
         X_train,
         y_train,
-        X_test,
-        y_test,
         optimizer,
         batch_size=100,
-        epochs=60,
+        epochs=30,
+        validation_split=0.2,
     ):
-        model.compile(
-            loss=tf.keras.losses.SparseCategoricalCrossentropy(),
-            optimizer=optimizer,
-            metrics=["accuracy"],
-        )
+        if optimizer:
+            model.compile(
+                loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+                optimizer=optimizer,
+                metrics=["accuracy"],
+            )
+        else:
+            model.compile(
+                loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+                metrics=["accuracy"],
+            )
+            
         history = model.fit(
             X_train,
             y_train,
             batch_size=batch_size,
             epochs=epochs,
             shuffle=True,
-            validation_data=(X_test, y_test),
+            validation_split=validation_split
         )
 
-        train_accuracy = history.history["accuracy"][-1]
-        val_accuracy = history.history["val_accuracy"][-1]
-
-        return history, train_accuracy, val_accuracy
+        return history
 
     @staticmethod
     def evaluate(
         model,
         X_test,
         y_test,
+        history
     ):
+        train_accuracy = history.history["accuracy"][-1]
+        val_accuracy = history.history["val_accuracy"][-1]
+    
         loss, accuracy = model.evaluate(X_test, y_test, verbose=2)
-        return loss, accuracy
+
+        print(f"\nTraining accuracy : {train_accuracy}")
+        print(f"Validation accuracy : {val_accuracy}")
+        print(f"Loss : {loss}")
+        print(f"Accuracy : {accuracy}\n")
+
+        return loss, accuracy, train_accuracy, val_accuracy
 
     @staticmethod
-    def plot_history(history, optimizer):
-        plt.plot(history.history["loss"], label="Training loss")
-        plt.plot(history.history["val_loss"], label="Validation loss")
-        plt.title(f"Training and Validation Loss ({optimizer.__class__.__name__})")
-        plt.xlabel("Epochs")
-        plt.ylabel("Loss")
+    def plot_history(history):
+       
+        # Plot training & validation accuracy values
+        plt.figure(figsize=(12,  6))
+        plt.subplot(1,  2,  1)
+        plt.plot(history.history['accuracy'], label='Training Accuracy')
+        plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+        plt.title(f'Model Accuracy')
+        plt.ylabel('Accuracy')
+        plt.xlabel('Epoch')
         plt.legend()
+
+        # Plot training & validation loss values
+        plt.subplot(1,  2,  2)
+        plt.plot(history.history['loss'], label='Training Loss')
+        plt.plot(history.history['val_loss'], label='Validation Loss')
+        plt.title(f'Model Loss')
+        plt.ylabel('Loss')
+        plt.xlabel('Epoch')
+        plt.legend()
+
+        plt.show()
+
+    @staticmethod
+    def plot_analysis_results(analysis, x_axis, y_axis):
+        all_results = analysis.trial_dataframes
+       
+        plt.figure(figsize=(12, 6))
+        for trial in all_results.values():
+            plt.plot(trial[f"{x_axis}"], trial[f"{y_axis}"], marker='o', label=f"Trial {trial['trial_id'][0]}")
+
+        plt.title(f'{y_axis} vs {x_axis}')
+        plt.xlabel(f"{x_axis}")
+        plt.ylabel(f"{y_axis}")
+        plt.legend()
+        plt.grid(True)
         plt.show()
